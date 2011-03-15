4fc1b5a304faceb04345f4d9800f4258dd2b252c
Ketan <str8ketan@gmail.com>
2011-03-14 22:59:04 -0400

    logo helper and rspec test to see if all links work on root page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
 
-  #Return a title on a per-page basis
+  # abstracts out the site logo in _header
+  def logo
+    logo = image_tag("rails.png", :alt => "Sample App", :class => "round")
+  end
+
+  # Return a title on a per-page basis
   def title                                             # Method definition
     base_title = "Ruby on Rails Tutorial Sample App"    # Variable assignment
     if @title.nil?                                      # Boolean test for nil
