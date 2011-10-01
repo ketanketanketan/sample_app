50c32b2707e7efd2b85ae78961c7f9db786f1a33
Ketan Patel <str8ketan@gmail.com>
2011-10-01 13:34:14 -0400

    Added Sign Up form and fixed some rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,5 +36,16 @@ describe UsersController do
     
   end
   
+  describe "GET 'new'" do
+    it "should be successful" do
+      get :new
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :new
+      response.should have_selector("title", :content => "Sign Up")
+    end
+  end
 
 end
