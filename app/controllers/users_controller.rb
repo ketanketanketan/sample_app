0385ca702a178348e2cbc1b49ca58a18c6a30fbc
Ketan <str8ketan@gmail.com>
2012-03-05 05:35:49 -0500

    10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def show 
     @user = User.find(params[:id])
     @title = @user.name
@@ -26,13 +29,10 @@ class UsersController < ApplicationController
   end #end create
 
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
   
   def update
-    
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated."
       redirect_to @user
@@ -40,7 +40,17 @@ class UsersController < ApplicationController
       @title = "Edit user"
       render 'edit'
     end
-
   end
 
+  private 
+    
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
 end
